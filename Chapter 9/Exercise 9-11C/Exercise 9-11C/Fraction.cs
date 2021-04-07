8227ba92b908f6a8dd59ea8b3a9f09967226b348
HauXun <hautrieu65@gmail.com>
2021-04-07 22:37:20 +0700

    Additional Ex 9-11C

---
+++ Chapter 9/Exercise 9-11C/Exercise 9-11C/Fraction.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Exercise_9_11C
+{
+	class Fraction
+	{
+		int wholeNumber;
+		int numerator;
+		int denominator;
+
+		public Fraction(int wholeNumber, int numerator, int denominator)
+		{
+			this.wholeNumber = wholeNumber;
+			this.numerator = numerator;
+			this.denominator = denominator;
+		}
+
+		public Fraction(int numerator, int denominator) : this(0, numerator, denominator)
+		{
+
+		}
+
+		public Fraction() : this(0, 0, 1)
+		{
+
+		}
+
+		public int WholeNumber
+		{
+			get => wholeNumber;
+			set
+			{
+				wholeNumber = value;
+			}
+		}
+
+		public int Numerator
+		{
+			get => numerator;
+			set
+			{
+				numerator = value;
+			}
+		}
+
+		public int Denominator
+		{
+			get => denominator;
+			set
+			{
+				if (value != 0)
+					denominator = value;
+				else
+					denominator = 1;
+			}
+		}
+
+		public static Fraction operator +(Fraction f1, Fraction f2)
+		{
+			int num1 = (f1.wholeNumber * f1.denominator + f1.numerator) * f2.denominator;
+			int num2 = (f2.wholeNumber * f2.denominator + f2.numerator) * f1.denominator;
+			int num = num1 + num2;
+			int denom = f1.denominator * f2.denominator;
+			Fraction newFrac = new Fraction(num, denom);
+			newFrac.Reduce();
+			return newFrac;
+		}
+
+		public static Fraction operator *(Fraction f1, Fraction f2)
+		{
+			int num1 = (f1.wholeNumber * f1.denominator + f1.numerator);
+			int num2 = (f2.wholeNumber * f2.denominator + f2.numerator);
+			int num = num1 * num2;
+			int denom = f1.denominator * f2.denominator;
+			Fraction newFrac = new Fraction(num, denom);
+			newFrac.Reduce();
+			return newFrac;
+		}
+
+		public void Reduce()
+		{
+			int gcd;
+			if (numerator >= denominator)
+			{
+				wholeNumber += numerator / denominator;
+				numerator = numerator % denominator;
+			}
+			gcd = 1;
+			for (int i = numerator; i > 0; i--)
+			{
+				if (numerator % i == 0 && denominator % i == 0)
+				{
+					gcd = i;
+					i = 0;
+				}
+			}
+			numerator /= gcd;
+			denominator /= gcd;
+		}
+
+		public string FracString()
+		{
+			string fracString;
+			if (wholeNumber == 0 && numerator == 0)
+				fracString = "0";
+			else if (wholeNumber == 0)
+				fracString = numerator + "/" + denominator;
+			else if (numerator == 0)
+				fracString = "" + wholeNumber;
+			else
+				fracString = wholeNumber + " " + numerator + "/" + denominator;
+			return fracString;
+		}
+	}
+}
---
+++ Chapter 9/Exercise 9-11C/Exercise 9-11C/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+Write a program named FractionDemo3 that includes an array of four
+Fractions. Prompt the user for values for each. Display every possible
+combination of addition results and every possible combination of multiplication
+results for each Fraction pair (that is, each type will have 16 results).
+ */
+
+
+
+
+using System;
+using static System.Console;
+
+namespace Exercise_9_11C
+{
+	class FractionDemo3
+	{
+		static void Main(string[] args)
+		{
+			Fraction[] f = new Fraction[4];
+			Fraction answer = new Fraction();
+			for (int i = 0; i < f.Length; i++)
+			{
+				f[i] = new Fraction();
+				GetData(f[i]);
+			}
+			WriteLine("".PadRight(10, '-') + "Addittion" + "".PadRight(10, '-'));
+			for (int i = 0; i < f.Length; i++)
+			{
+				for (int j = 0; j < f.Length; j++)
+				{
+					answer = f[i] + f[j];
+					WriteLine("{0} + {1} = {2}", f[i].FracString(), f[j].FracString(), answer.FracString());
+				}
+			}
+			WriteLine("\n".PadRight(10, '-') + "Multiplication" + "".PadRight(10, '-'));
+			for (int i = 0; i < f.Length; i++)
+			{
+				for (int j = 0; j < f.Length; j++)
+				{
+					answer = f[i] * f[j];
+					WriteLine("{0} * {1} = {2}", f[i].FracString(), f[j].FracString(), answer.FracString());
+				}
+			}
+		}
+
+		static void GetData(Fraction f)
+		{
+			int temp;
+			Write("Enter whole number portion of fraction >> ");
+			int.TryParse(ReadLine(), out temp);
+			f.WholeNumber = temp;
+			Write("Enter numerator >> ");
+			int.TryParse(ReadLine(), out temp);
+			f.Numerator = temp;
+			Write("Enter denominator >> ");
+			int.TryParse(ReadLine(), out temp);
+			f.Denominator = temp;
+			WriteLine("\n".PadRight(5, '-') + "Enter complete" + "".PadRight(5, '-'));
+		}
+	}
+}
