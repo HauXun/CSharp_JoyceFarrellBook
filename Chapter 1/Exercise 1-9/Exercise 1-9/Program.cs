304384826a000f6417fc49cce6a482259301ba05
HauXun <hautrieu65@gmail.com>
2021-04-16 21:09:16 +0700

    Init Commit 🚀🚀🚀

---
+++ Chapter 1/Exercise 1-9/Exercise 1-9/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+From 1925 through 1963, Burma Shave advertising signs appeared next to highways
+all across the United States. There were always four or five signs in a row containing
+pieces of a rhyme, followed by a final sign that read “Burma Shave.” For example, one
+set of signs that has been preserved by the Smithsonian Institution reads as follows:
+
+Shaving brushes
+You’ll soon see ’em
+On a shelf
+In some museum
+Burma Shave
+
+Find a classic Burma Shave rhyme on the Web and write a program named
+BurmaShave that displays the rhyme.
+ */
+
+
+
+using System;
+
+namespace Exercise_1_9
+{
+    class BurmaShave
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tBurmaShave\n");
+            Console.WriteLine("\tBurma Shave Rhymes");
+            Console.WriteLine("\t IF YOU DISK LIKE");
+            Console.WriteLine("\t BIG TRAFFIC FINES");
+            Console.WriteLine("\t    SLOW DOWN");
+            Console.WriteLine("\t     TIL YOU");
+            Console.WriteLine("\t    CAN READ");
+            Console.WriteLine("\t   THESE SIGNS");
+            Console.WriteLine("\t   Burma-Shave");
+            Console.WriteLine("\nProgram Test Done...!");
+            Console.ReadLine();
+        }
+    }
+}
