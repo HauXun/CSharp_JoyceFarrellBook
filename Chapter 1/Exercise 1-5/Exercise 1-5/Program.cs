173042b90923b05a31ad687ba1e4f8573eef254d
HauXun <hautrieu65@gmail.com>
2021-04-16 21:08:22 +0700

    Init Commit 🚀🚀🚀

---
+++ Chapter 1/Exercise 1-5/Exercise 1-5/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Write, compile, and test a program named Lyrics that displays at least four lines of
+your favorite song.
+ */
+
+
+using System;
+
+namespace Exercise_1_5
+{
+    class Lyrics
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tLyrics");
+            Console.WriteLine("Happy Birthday DaLat");
+            Console.WriteLine("Happy Birthday to you");
+            Console.WriteLine("Happy Birthday to everyone");
+            Console.WriteLine("Hope you like this program.");
+            Console.WriteLine("\nProgram Test Done...!");
+            Console.ReadLine();
+        }
+    }
+}
