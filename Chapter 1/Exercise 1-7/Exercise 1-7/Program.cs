68f5d88a78d971ec8aa6c4326fed2d59c96b68c4
HauXun <hautrieu65@gmail.com>
2021-04-16 21:08:52 +0700

    Init Commit 🚀🚀🚀

---
+++ Chapter 1/Exercise 1-7/Exercise 1-7/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Write, compile, and test a program named StopSign that displays a pattern similar to
+the image shown in Figure 1-19.
+ */
+
+
+using System;
+
+namespace Exercise_1_7
+{
+    class StopSign
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tStopSign");
+            Console.WriteLine("  XXXXXX");
+            Console.WriteLine(" X\tX");
+            Console.WriteLine("X  STOP  X");
+            Console.WriteLine(" X\tX");
+            Console.WriteLine("  XXXXXX");
+            Console.WriteLine("     X");
+            Console.WriteLine("     X");
+            Console.WriteLine("     X");
+            Console.WriteLine("\nProgram Test Done...!");
+            Console.ReadLine();
+        }
+    }
+}
