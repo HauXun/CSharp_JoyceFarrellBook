e1186be5f2607b203ac7f6bdd7d16f1c07eb3d68
HauXun <hautrieu65@gmail.com>
2021-04-16 21:08:36 +0700

    Init Commit 🚀🚀🚀

---
+++ Chapter 1/Exercise 1-6/Exercise 1-6/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Write, compile, and test a program named Comments that displays a statement
+that defines program comments. Include at least one block comment and one line
+comment in the program.
+ */
+
+
+using System;
+
+namespace Exercise_1_6
+{
+    class Comments
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tComments");
+            Console.WriteLine("Was Comments...");
+
+            /* Program: Comments
+             * Created by: Trieu Trong Hau
+             * Created on: 25/2/2021*/
+
+            Console.WriteLine("\nProgram Test Done...!");
+            Console.ReadLine();
+
+            //Thank you for using this program
+        }
+    }
+}
