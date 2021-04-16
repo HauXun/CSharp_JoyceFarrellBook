c1cd457816d7fc483d528d205fae1317b9488057
HauXun <hautrieu65@gmail.com>
2021-04-16 21:07:59 +0700

    Init Commit 🚀🚀🚀

---
+++ Chapter 1/Exercise 1-4/Exercise 1-4/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Write, compile, and test a program named PersonalInfo that displays a person’s
+ * name, birthdate, work phone number, and cell phone number.
+ */
+
+using System;
+
+namespace Exercise_1_4
+{
+    class PersonalInfo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\tPersonalInfo");
+            Console.WriteLine("Name: Trieu Trong Hau");
+            Console.WriteLine("Date of Birth: 07/05/2002");
+            Console.WriteLine("Work PhoneNumber: 1122334455");
+            Console.WriteLine("Cell PhoneNumber: 0852987191");
+            Console.WriteLine("\nProgram Test Done...!");
+            Console.ReadLine();
+        }
+    }
+}
