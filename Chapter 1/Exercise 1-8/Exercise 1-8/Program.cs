0a08973332acaf6a7c2db225142e0a8a640a1837
HauXun <hautrieu65@gmail.com>
2021-04-16 21:09:04 +0700

    Init Commit 🚀🚀🚀

---
+++ Chapter 1/Exercise 1-8/Exercise 1-8/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Write a program named BigLetter that displays a large letter composed of smaller
+letters. For example, if you choose H, then the output should look similar to the image
+shown in Figure 1-20.
+ */
+
+
+using System;
+
+namespace Exercise_1_8
+{
+    class BigLetter
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tBigLetter");
+            Console.WriteLine("\tL");
+            Console.WriteLine("\tL");
+            Console.WriteLine("\tL");
+            Console.WriteLine("\tL");
+            Console.WriteLine("\tL");
+            Console.WriteLine("\tL");
+            Console.WriteLine("\tL");
+            Console.WriteLine("\tLLLLLLLLL");
+            Console.WriteLine("\nProgram Test Done...!");
+            Console.ReadLine();
+        }
+    }
+}
