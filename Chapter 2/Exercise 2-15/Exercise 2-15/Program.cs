848b5f24c374ab68fa1dc8c5f22f534109ec3552
HauXun <hautrieu65@gmail.com>
2021-05-05 20:06:04 +0700

    Additional

---
+++ Chapter 2/Exercise 2-15/Exercise 2-15/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Write a program named FahrenheitToCelsius that accepts a temperature in
+Fahrenheit from a user and converts it to Celsius by subtracting 32 from the Fahrenheit
+value and multiplying the result by 5/9. Display both values to one decimal place.
+ */
+
+
+
+using System;
+
+namespace Exercise_2_15
+{
+    class FahrenheitToC
+    {
+        static void Main(string[] args)
+        {
+            double Celsius;
+            Console.Write("Enter the Fahrenheit Temperature: ");
+            double Fahrenheit = double.Parse(Console.ReadLine());
+            Celsius = (Fahrenheit - 32) * 5 / 9;
+            Console.WriteLine("The Converted Celsius Temperature is: {0:n1}", Celsius);
+            Console.ReadLine();
+        }
+    }
+}
