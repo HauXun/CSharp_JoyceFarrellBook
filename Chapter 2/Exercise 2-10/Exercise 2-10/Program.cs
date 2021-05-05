500200b47f3f05c678aa663cc444f725072f6e7e
HauXun <hautrieu65@gmail.com>
2021-05-05 20:05:25 +0700

    Additional

---
+++ Chapter 2/Exercise 2-10/Exercise 2-10/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Write a program named HoursAndMinutes that declares a minutes variable to
+represent minutes worked on a job, and assign a value to it. Display the value in hours
+and minutes. For example, 197 minutes becomes 3 hours and 17 minutes.
+ */
+
+
+
+using System;
+
+namespace Exercise_2_10
+{
+    class HoursAndMinutes
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your total time: ");
+            int totalTime = int.Parse(Console.ReadLine());
+
+            int hours = totalTime / 60;
+            int minutes = totalTime % 60;
+
+            Console.WriteLine("The value in hours and minutes: {0} hours + {1} minutes", hours, minutes);
+            Console.ReadLine();
+        }
+    }
+}
