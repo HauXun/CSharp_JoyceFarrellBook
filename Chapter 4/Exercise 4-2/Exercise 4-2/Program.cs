196c9034bf282ed7ce2c243e0c97aec148f6d582
HauXun <hautrieu65@gmail.com>
2021-06-10 20:05:06 -0700

    Additional Ex

---
+++ Chapter 4/Exercise 4-2/Exercise 4-2/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Write a program named Twitter that accepts a user’s message and determines
+whether it is short enough for a social networking service that does not accept
+messages of more than 140 characters.
+ */
+
+
+
+using System;
+
+namespace Exercise_4_2
+{
+    class Twitter
+    {
+        static void Main(string[] args)
+        {
+            const int MAX = 140;
+            Console.Write("Enter a message to check: ");
+            string messages = Console.ReadLine();
+            if (messages.Length <= MAX)
+                Console.WriteLine("Yes, it's short enough for Twitter");
+            else
+                Console.WriteLine("Sorry your messages was too long");
+            Console.ReadLine();
+        }
+    }
+}
