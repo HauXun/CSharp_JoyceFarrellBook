3fd8e18456c231782428b56e9687ef7fe59d29e5
HauXun <hautrieu65@gmail.com>
2021-10-22 21:27:44 +0700

    Additional Ex 5-1

---
+++ Chapter 5/Exercise 5-1/Exercise 5-1/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Write an application named SumFiveInts that allows the user to enter five integers
+and displays their sum.
+ */
+
+
+
+using System;
+
+namespace Exercise_5_1
+{
+    class SumFiveInts
+    {
+        static void Main(string[] args)
+        {
+            int[] integer = new int[5];
+            int sum = 0;
+            Console.WriteLine("Enter 5 integers...\n");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("No.{0} Enter the integer: ", i + 1);
+                integer[i] = int.Parse(Console.ReadLine());
+            }
+            foreach (var item in integer)
+            {
+                sum += item;
+            }
+            Console.WriteLine("\nSum of entered 5 integer numbers is: {0}", sum);
+            Console.ReadLine();
+        }
+    }
+}
