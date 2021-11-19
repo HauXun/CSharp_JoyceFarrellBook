0a728621af5b31798b0c8873800ab5c020ac4b62
HauXun <hautrieu65@gmail.com>
2021-11-19 22:27:29 +0700

    Additional Ex 8-9

---
+++ Chapter 8/Exercise 8-9/Exercise 8-9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise_8_9
+{
+	class Movie
+	{
+		static void Main(string[] args)
+		{
+			Random minutes = new Random();
+
+			//Write("Enter you movie name: ");
+			//string title = ReadLine();
+
+			//DisplayMovie(title, minutes.Next(60, 300));
+
+			DisplayMovie("The temple of Light", minutes.Next(60, 300));
+			DisplayMovie("Avenger (End Game)", minutes.Next(60, 300));
+		}
+
+		public static void DisplayMovie(string title, int minutes)
+		{
+			WriteLine($"The movie {title} has a runing time of {minutes} minutes!");
+		}
+	}
+}
