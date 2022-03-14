a5f09d3a7448b45d7d9bcb1ae61a1c21976a5773
HauXun <hautrieu65@gmail.com>
2022-03-14 23:27:34 +0700

    Additional Ex 5-6

---
+++ Chapter 5/Exercise 5-6/Exercise 5-6/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Write an application named DisplayMultiplicationTable that displays a table of the
+products of every combination of two integers from 1 through 10.
+ */
+
+
+
+using System;
+using static System.Console;
+
+namespace Exercise_5_6
+{
+	class DisplayMultiplicationTable
+	{
+		static void Main(string[] args)
+		{
+			int first, second;
+			const int LIMIT = 10;
+			Write("     ");
+			for (first = 1; first <= LIMIT; first++)
+				Write("{0, 5}", first);
+			WriteLine();
+			WriteLine("".PadRight(55, '-'));
+			WriteLine();
+			for (first = 1; first <= LIMIT; first++)
+			{
+				Write("{0, 5}", first);
+				for (second = 1; second <= LIMIT; second++)
+					Write("{0, 5}", first * second);
+				WriteLine();
+			}
+		}
+	}
+}
