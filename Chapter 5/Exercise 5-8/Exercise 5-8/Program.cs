d0c832c32d1d2598cc920babe27f7941ed8c22ab
HauXun <hautrieu65@gmail.com>
2022-03-16 21:22:56 +0700

    Additional Ex 5-8

---
+++ Chapter 5/Exercise 5-8/Exercise 5-8/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Write an application named OddNums that displays all the odd numbers from 1
+through 99.
+ */
+
+
+
+using System;
+using static System.Console;
+
+namespace Exercise_5_8
+{
+    class OddNums
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Odd numbers from 1 through 99.\n\n");
+
+            for (int i = 1; i <= 99; i++)
+                if (i % 2 != 0)
+                    Write("{0, 5}", i);
+
+            WriteLine("\n\n");
+        }
+    }
+}
