6a03b46369cc59be43e490683629f7309a5156c3
HauXun <hautrieu65@gmail.com>
2022-03-17 21:52:21 +0700

    Additional Ex 5-9

---
+++ Chapter 5/Exercise 5-9/Exercise 5-9/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Write an application named Sum200 that sums the integers from 1 through 200.
+Display the running total when the program is halfway complete (after the first 100
+numbers), and at the end.
+ */
+
+
+
+using System;
+using static System.Console;
+
+namespace Exercise_5_9
+{
+    class Sum200
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Sums the integers from 1 through 200\n\n");
+
+            int i = 1;
+            int sum = 0;
+            while (i <= 200)
+            {
+                sum += i;
+                if (i <= 100)
+                    // Display the running total when the program is halfway complete (after the first 100 numbers)
+                    WriteLine("Sum is {0}", sum);
+                ++i;
+            }
+            WriteLine("\n\nTotal after the end of the program is {0}", sum);
+        }
+    }
+}
