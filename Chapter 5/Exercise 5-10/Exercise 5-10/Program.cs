32b202f1468566e2df217a7cc4449f3f67827424
HauXun <hautrieu65@gmail.com>
2022-03-18 22:04:12 +0700

    Additional Ex 5-10

---
+++ Chapter 5/Exercise 5-10/Exercise 5-10/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Write an application named Perfect that displays every perfect number from 1
+through 10,000. A number is perfect if it equals the sum of all the smaller positive
+integers that divide evenly into it. For example, 6 is perfect because 1, 2, and 3 divide
+evenly into it and their sum is 6
+ */
+
+
+
+using System;
+using static System.Console;
+
+namespace Exercise_5_10
+{
+    class Perfect
+    {
+        static void Main(string[] args)
+        {
+            int sum;
+            WriteLine("Here are all the perfect numbers from 1 to 10000\n\n");
+            for (int n = 1; n <= 10000; n++)
+            {
+                sum = 0;
+                for (int i = 1; i < n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        sum += i;
+                    }
+                }
+                if (sum == n)
+                {
+                    WriteLine("{0} is a perfect number", sum);
+                }
+            }
+        }
+    }
+}
