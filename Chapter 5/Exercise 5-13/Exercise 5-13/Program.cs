134ca736628a620efd4ea7ccaaa665cc7c060d8e
HauXun <hautrieu65@gmail.com>
2022-03-21 23:33:22 +0700

    Additional Ex 5-13

---
+++ Chapter 5/Exercise 5-13/Exercise 5-13/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Write a program named CountVowels that accepts a phrase from the user and counts
+the number of vowels in the phrase. For this exercise, count both uppercase and
+lowercase vowels, but do not consider y to be a vowel.
+ */
+
+
+
+using System;
+using static System.Console;
+
+namespace Exercise_5_13
+{
+    class CountVowels
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            int vowelCount = 0;
+
+            Write("Please enter a word to see the amount of vowels it contains: ");
+            userInput = ReadLine().ToLower();
+
+            foreach (char letter in userInput)
+                if ("aeiou".Contains(letter))
+                    vowelCount++;
+            WriteLine("\n{0} contains {1} vowels", userInput, vowelCount);
+        }
+    }
+}
