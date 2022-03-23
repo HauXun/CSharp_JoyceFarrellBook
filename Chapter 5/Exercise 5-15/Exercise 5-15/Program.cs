ecb3b6d392803a67315eac8200f6c37192bf2f0d
HauXun <hautrieu65@gmail.com>
2022-03-23 21:33:48 +0700

    Additional Ex 5-15

---
+++ Chapter 5/Exercise 5-15/Exercise 5-15/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+Modify the GuessingGame2 program to create a program called GuessingGame3
+in which the player is criticized for making a “dumb” guess. For example, if the player
+guesses that the random number is 4 and is told that the guess is too low, and then the
+player subsequently makes a guess lower than 4, display a message that the user should
+have known not to make such a low guess.
+ */
+
+
+
+using System;
+using static System.Console;
+
+namespace Exercise_5_15
+{
+    class GuessingGame3
+    {
+        static void Main(string[] args)
+        {
+            const int MIN = 1;
+            const int MAX = 16;
+            int randomNumber;
+            Nullable<int> preInput = null;
+            int input = -1;
+            int guesses = 1;
+
+            Random numberGenerator = new Random();
+            randomNumber = numberGenerator.Next(MIN, MAX);
+            while (input != randomNumber)
+            {
+                Write("Guess a number ({0} - {1}): ", MIN, MAX - 1);
+                input = int.Parse(ReadLine());
+                if (input == randomNumber)
+                {
+                    WriteLine("\nCorrect! {0} guesses.", guesses);
+                }
+                else if (input < randomNumber)
+                {
+                    WriteLine("Too low");
+                    guesses++;
+                    if (preInput != null && input < preInput && preInput < randomNumber)
+                        WriteLine("You are so stupid, don't guess so low.");
+                }
+                else
+                {
+                    WriteLine("Too high");
+                    guesses++;
+                    if (preInput != null && input > preInput && preInput > randomNumber)
+                        WriteLine("You are so stupid, don't guess so higher.");
+                }
+                preInput = input;
+            }
+        }
+    }
+}
