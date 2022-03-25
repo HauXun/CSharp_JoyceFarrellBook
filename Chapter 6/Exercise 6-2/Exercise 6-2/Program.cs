7f7fca7f8b2201de3aec9611c4ddf0ac95638d09
HauXun <hautrieu65@gmail.com>
2022-03-25 21:35:50 +0700

    Additional Ex 6-2

---
+++ Chapter 6/Exercise 6-2/Exercise 6-2/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Write a program named TestScoreList that accepts eight int values representing
+student test scores. Display each of the values along with a message that indicates how
+far it is from the average
+ */
+
+
+
+using System;
+using static System.Console;
+
+namespace Exercise_6_2
+{
+	class TestScoreList
+	{
+		static void Main(string[] args)
+		{
+			int[] score = new int[8];
+			double sum = 0;
+			for (int i = 0; i < 8; i++)
+			{
+				Write("{0} - Please enter scores for the test: ", i + 1);
+				score[i] = int.Parse(ReadLine());
+				sum += score[i];
+			}
+			double avg = sum / 8;
+			for (int i = 0; i < 8; i++)
+				WriteLine("Test #{0}: \t{1} ways the average is {2}", i + 1, score[i], score[i] - avg);
+			WriteLine("Total is: {0}", sum);
+			WriteLine("Average is: {1}", avg);
+			ReadLine();
+		}
+	}
+}
