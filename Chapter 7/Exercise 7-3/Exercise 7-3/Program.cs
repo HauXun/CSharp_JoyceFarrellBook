b3a18eb08bf55532160ba54d44aa3bfc88b3e39c
HauXun <hautrieu65@gmail.com>
2022-04-04 21:41:16 +0700

    Additional Ex 7-3

---
+++ Chapter 7/Exercise 7-3/Exercise 7-3/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Create a program named PaintingEstimate whose Main() method prompts a user
+for length and width of a room in feet. Create a method that accepts the values and
+then computes the cost of painting the room, assuming the room is rectangular and
+has four full walls and 9-foot ceilings. The price of the job is $6 per square foot. Return
+the price to the Main() method, and display it.
+ */
+
+
+
+
+using System;
+using static System.Console;
+
+namespace Exercise_7_3
+{
+	class PaintingEstimate
+	{
+		static void Main(string[] args)
+		{
+			Write("Enter the length of the room (in feet): ");
+			int length = int.Parse(ReadLine());
+			Write("Enter the width of the room (in feet): ");
+			int width = int.Parse(ReadLine());
+			ComputeCost(length, width);
+		}
+
+		public static void ComputeCost(int length, int width)
+
+		{
+			double cost = ((length * 9 * 2) + (width * 9 * 2)) * 6;
+			WriteLine($"\nThe cost of painting your room is {cost.ToString("c")}$");
+		}
+	}
+}
