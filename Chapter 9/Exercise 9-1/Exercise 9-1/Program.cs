21876b565dfeb237b35a0ac7f7723f65fa9c1cd5
HauXun <hautrieu65@gmail.com>
2022-07-24 22:20:24 +0700

    Additional Ex 9-1

---
+++ Chapter 9/Exercise 9-1/Exercise 9-1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Create an application named TestSoccerPlayer that instantiates and displays a
+SoccerPlayer object. The SoccerPlayer class contains properties that hold a
+player’s name (a string), jersey number (an integer), goals scored (an integer), and
+assists (an integer).
+ */
+
+
+
+
+using System;
+using static System.Console;
+
+namespace Exercise_9_1
+{
+	class TestSoccerPlayer
+	{
+		static void Main(string[] args)
+		{
+			SoccerPlayer myPlayer = new SoccerPlayer();
+			myPlayer.name = "Tran Dan";
+			myPlayer.jerseyNumber = 10;
+			myPlayer.goals = 23;
+			myPlayer.assists = 4;
+			WriteLine("The soccer player is {0}. Jersey is #{1} ", myPlayer.name, myPlayer.jerseyNumber);
+			WriteLine("Goals: {0}, Assists: {1}", myPlayer.goals, myPlayer.assists);
+		}
+	}
+}
---
+++ Chapter 9/Exercise 9-1/Exercise 9-1/SoccerPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise_9_1
+{
+	class SoccerPlayer
+	{
+		public string name { get; set; }
+		public int jerseyNumber { get; set; }
+		public int goals { get; set; }
+		public int assists { get; set; }
+	}
+}
