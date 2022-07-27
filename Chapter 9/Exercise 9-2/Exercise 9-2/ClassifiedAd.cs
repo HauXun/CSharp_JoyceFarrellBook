3da45b39459518ec3513730173735eb54bda3f70
HauXun <hautrieu65@gmail.com>
2022-07-27 23:00:11 +0700

    Additional Ex 9-2

---
+++ Chapter 9/Exercise 9-2/Exercise 9-2/ClassifiedAd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise_9_2
+{
+	class ClassifiedAd
+	{
+		const double PRICE_PER_WORD = 0.9;
+		public int words;
+		public double price;
+		public string Category { get; set; }
+		public int Words
+		{
+			get => words;
+			set
+			{
+				words = value;
+				price = words * PRICE_PER_WORD;
+			}
+		}
+
+		public double Price
+		{
+			get => price;
+		}
+	}
+}
---
+++ Chapter 9/Exercise 9-2/Exercise 9-2/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Create an application named TestClassifiedAd that instantiates and displays at least
+two ClassifiedAd objects. A ClassifiedAd has fields for a category (for example,
+Used Cars), a number of words, and a price. Include properties that contain get and
+set accessors for the category and number of words, but only a get accessor for the
+price. The price is calculated at nine cents per word.
+ */
+
+
+
+
+using System;
+using static System.Console;
+
+namespace Exercise_9_2
+{
+	class TestClassifiedAd
+	{
+		static void Main(string[] args)
+		{
+			ClassifiedAd myClassifiedAd = new ClassifiedAd();
+			ClassifiedAd yourClassifiedAd = new ClassifiedAd();
+			myClassifiedAd.Category = "Used cars";
+			myClassifiedAd.Words = 100;
+			yourClassifiedAd.Category = "Help wanted";
+			yourClassifiedAd.Words = 60;
+			WriteLine("The classified ad with {0} words in category {1} costs {2}", myClassifiedAd.Words, myClassifiedAd.Category, myClassifiedAd.price.ToString("c"));
+			WriteLine("The classified ad with {0} words in category {1} costs {2}", yourClassifiedAd.Words, yourClassifiedAd.Category, yourClassifiedAd.price.ToString("c"));
+		}
+	}
+}
