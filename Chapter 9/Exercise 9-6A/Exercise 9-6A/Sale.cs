0e9c54c8ddcec8081785dbf40e0a2c3ce8b8b52b
HauXun <hautrieu65@gmail.com>
2022-07-31 22:30:26 +0700

    Additional Ex 9-6A

---
+++ Chapter 9/Exercise 9-6A/Exercise 9-6A/Sale.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exercise_9_6A
+{
+	class Sale
+	{
+		string inventoryNumber;
+		double amount;
+		double tax;
+
+		public string InventoryNumber
+		{
+			get => inventoryNumber;
+			set
+			{
+				inventoryNumber = value;
+			}
+		}
+
+		public double Amount 
+		{
+			get => amount;
+			set
+			{
+				amount = value;
+				CalculateTax();
+			}
+		}
+
+		public double GetTax
+		{
+			get => tax;
+		}
+
+		void CalculateTax()
+		{
+			if (amount > 100.0)
+				tax = 0.08 * 100 + 0.06 * (amount - 100.0);
+			else
+				tax = 0.08 * amount;
+		}
+	}
+}
+
