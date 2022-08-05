6b319492751a6131651801245eef6420e33aed00
HauXun <hautrieu65@gmail.com>
2022-08-05 22:47:01 +0700

    Additional Ex 9-6B

---
+++ Chapter 9/Exersice 9-6B/Exersice 9-6B/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+Create a program named SalesTaxDemo2 so that after the 10 Sale objects are
+displayed, they are sorted in order by the amount of tax owed and displayed again.
+Modify the Sale class so its objects are comparable to each other based on tax
+owed.
+ */
+
+
+
+
+using System;
+using static System.Console;
+
+namespace Exersice_9_6B
+{
+	class SalesTaxDemo2
+	{
+		static void Main(string[] args)
+		{
+			// Number of objects
+			int time = 10;
+			Sale[] sale = new Sale[10];
+
+			for (int i = 0; i < 10; i++)
+				sale[i] = new Sale();
+			for (int i = 0; i < time; i++)
+			{
+				Write("Enter inventory number #{0} >> ", i + 1);
+				sale[i].InventoryNumber = ReadLine();
+				Write("Enter amount of sale >> ");
+				sale[i].Amount = double.Parse(ReadLine());
+			}
+			WriteLine("\n");
+
+			// Printing details of every sale
+			for (int i = 0; i < time; i++)
+				WriteLine("Sale #{0} Amount: {1}\tSale: ${2}\tTax is ${3}", i + 1, sale[i].InventoryNumber,
+					String.Format("{0:n}", sale[i].Amount), String.Format("{0:n}", sale[i].GetTax));
+
+			WriteLine("\n");
+
+			// Sorted
+			Array.Sort(sale);
+			WriteLine("".PadRight(10, '-') + "Sorted".PadRight(10, '-'));
+			for (int i = 0; i < time; i++)
+				WriteLine("Sale #{0} Amount: {1}\tSale: ${2}\tTax is ${3}", i + 1, sale[i].InventoryNumber,
+					String.Format("{0:n}", sale[i].Amount), String.Format("{0:n}", sale[i].GetTax));
+		}
+	}
+}
