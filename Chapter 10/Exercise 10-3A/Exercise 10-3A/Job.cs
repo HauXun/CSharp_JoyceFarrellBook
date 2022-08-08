dff244c78ca2cd9112d70a0b16fd5b6592557ba0
HauXun <hautrieu65@gmail.com>
2022-08-08 22:50:20 +0700

    Additional Ex 10-3A

---
+++ Chapter 10/Exercise 10-3A/Exercise 10-3A/Job.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_10_3A
+{
+	class Job
+	{
+		double hours;
+		double price;
+		const double RATE = 45.00;
+
+		public int JobNumber { get; set; }
+		public string Customer { get; set; }
+		public string Description { get; set; }
+		public double Hours
+		{
+			get => hours;
+			set
+			{
+				hours = value;
+				price = hours * RATE;
+			}
+		}
+
+		public double Price
+		{
+			get => price;
+		}
+
+		public Job(int num, string cust, string desc, double hrs)
+		{
+			JobNumber = num;
+			Customer = cust;
+			Description = desc;
+			Hours = hrs;
+		}
+
+		public override string ToString()
+		{
+			return (GetType() + " " + JobNumber + " " + Customer + " " + Description + " " + Hours +
+				" hours @" + RATE.ToString("c") + " per hour. Total price is " + Price.ToString("c"));
+		}
+
+		public override bool Equals(object obj)
+		{
+			bool equal;
+			Job temp = (Job)obj;
+			if (JobNumber == temp.JobNumber)
+				equal = true;
+			else
+				equal = false;
+			return equal;
+		}
+
+		public override int GetHashCode() => JobNumber;
+	}
+}
---
+++ Chapter 10/Exercise 10-3A/Exercise 10-3A/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+/*
+a. Create an application named JobDemo that declares and uses Job objects. The
+Job class holds job information for a home repair service. The class has five
+properties that include a job number, customer name, job description, estimated
+hours, and price for the job. Create a constructor that requires parameters for all
+the data except price. Include auto-implemented properties for the job number,
+customer name, and job description, but not for hours or price; the price field value
+is calculated as estimated hours times $45.00 whenever the hours value is set. Also
+create the following for the class:
+• An Equals() method that determines two Jobs are equal if they have the same
+job number
+• A GetHashCode() method that returns the job number
+• A ToString() method that returns a string containing all job information
+The JobDemo application declares a few Job objects, sets their values, and
+demonstrates that all the methods work as expected.
+*/
+
+
+
+
+
+using System;
+using static System.Console;
+
+namespace Exercise_10_3A
+{
+	class JobDemo
+	{
+		static void Main(string[] args)
+		{
+			Job job1 = new Job(111, "Smith", "Exterior Paint", 20);
+			Job job2 = new Job(222, "Vega", "Gutter Clean", 4);
+			Job job3 = new Job(111, "Land", "Black Drive", 10);
+			WriteLine(job1.ToString());
+			WriteLine(job2.ToString());
+			WriteLine(job3.ToString());
+			CompareNumber(job1, job2);
+			CompareNumber(job1, job3);
+		}
+		internal static void CompareNumber(Job job1, Job job2)
+		{
+			if (job1.Equals(job2))
+				WriteLine("{0} for {1} has the same job " + " number as " + "{2} for {3}",
+					job1.JobNumber, job1.Customer, job2.JobNumber, job2.Customer);
+		}
+	}
+}
