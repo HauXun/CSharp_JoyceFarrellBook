3a2ff1db078414d5dff4fff0ae22cbdaf4980fe9
HauXun <hautrieu65@gmail.com>
2022-08-10 23:08:38 +0700

    Additional Ex 10-3C

---
+++ Chapter 10/Exercise 10-3C/Exercise 10-3C/Program.cs
@@ -0,0 +1,65 @@
+﻿/*
+c. Create a RushJob class that derives from Job. A RushJob has a $150.00 premium
+that is added to the normal price of the job. Override any methods in the parent
+class as necessary. Write a new application named JobDemo3 that creates an array
+of five RushJobs. Prompt the user for values for each, and do not allow duplicate
+job numbers. When five valid RushJob objects have been entered, display them all,
+plus a total of all prices.
+ */
+
+
+
+
+
+using System;
+using static System.Console;
+
+namespace Exercise_10_3C
+{
+	class JobDemo3
+	{
+		static void Main(string[] args)
+		{
+			RushJob[] job = new RushJob[2];
+			double grandTotal = 0;
+			bool goodNumber;
+			for (int i = 0; i < job.Length; i++)
+			{
+				job[i] = new RushJob();
+				Write("Enter job number >> ");
+				job[i].JobNumber = int.Parse(ReadLine());
+				goodNumber = true;
+				for (int j = 0; j < i; j++)
+				{
+					if (job[i].Equals(job[j]))
+						goodNumber = false;
+				}
+				while (!goodNumber)
+				{
+					WriteLine("Sorry, the job number " + job[i].JobNumber + " is a dublicate. " + "\nPlease reenter >> ");
+					job[i].JobNumber = int.Parse(ReadLine());
+					goodNumber = true;
+					for (int j = 0; j < i; j++)
+					{
+						if (job[i].Equals(job[j]))
+							goodNumber = false;
+					}
+				}
+				Write("\nEnter customer name >> ");
+				job[i].Customer = ReadLine();
+				Write("\nEnter job description >> ");
+				job[i].Description = ReadLine();
+				Write("\nEnter estimated hours for job >> ");
+				job[i].Hours = double.Parse(ReadLine());
+				WriteLine("\n");
+			}
+			WriteLine("\n\t\t\tSUMARY\n");
+			for (int i = 0; i < job.Length; i++)
+			{
+				WriteLine(job[i].ToString());
+				grandTotal += job[i].Price;
+			}
+			WriteLine("\nTotal for all job is: " + grandTotal.ToString("c"));
+		}
+	}
+}
---
+++ Chapter 10/Exercise 10-3C/Exercise 10-3C/RushJob.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_10_3C
+{
+	class RushJob : Job, IComparable
+	{
+		public const double PREMIUM = 150.00;
+
+		public RushJob() : base(0, null, null, 0)
+		{
+		}
+
+		public override string ToString()
+		{
+			return (GetType() + " " + JobNumber + " " + Customer + " " + Description + " " + Hours +
+					" hours @" + RATE.ToString("C") +
+					" per hour. Rush job adds " + PREMIUM + " premium. Total price is " + Price.ToString("C"));
+		}
+
+		public new double Hours
+		{
+			get => hours;
+			set
+			{
+				hours = value;
+				price = hours * RATE + PREMIUM;
+			}
+		}
+
+		int IComparable.CompareTo(object obj)
+		{
+			RushJob temp = (RushJob)obj;
+			if (this.JobNumber > temp.JobNumber)
+				return 1;
+			else
+				if (this.JobNumber < temp.JobNumber)
+				return -1;
+			else
+				return 0;
+		}
+	}
+}
