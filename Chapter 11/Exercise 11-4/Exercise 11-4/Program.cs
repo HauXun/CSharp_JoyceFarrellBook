5d8af69a513fb2e67606bec380d7737a9f0adc23
HauXun <hautrieu65@gmail.com>
2022-08-24 22:14:54 +0700

    Additional Ex 11-4

---
+++ Chapter 11/Exercise 11-4/Exercise 11-4/Program.cs
@@ -0,0 +1,73 @@
+﻿/*
+Create a program named StudentGradeDemo that attempts to create several valid
+and invalid ReportCard objects. Immediately after each instantiation attempt, handle
+any thrown exceptions by displaying an error message. Create a ReportCard class
+with four fields for a student name, a numeric midterm grade, a numeric final exam
+grade, and letter grade. The ReportCard constructor requires values for the name
+and two numeric grades and determines the letter grade. Upon construction, throw
+an ArgumentException if the midterm or final exam grade is less than 0 or more
+than 100. The letter grade is based on the arithmetic average of the midterm and
+final exams using a grading scale of A for an average of 90 to 100, B for 80 to 90, C
+for 70 to 80, D for 60 to 70, and F for an average below 60. Display all the data if the
+instantiation is successful.
+ */
+
+
+
+
+using System;
+using static System.Console;
+
+namespace Exercise_11_4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				ReportCard reportCard1 = new ReportCard("Adams", 80, 85);
+			}
+			catch (ArgumentException e)
+			{
+				WriteLine("{0} {1}", "reportCard1", e.Message);
+			}
+
+			try
+			{
+				ReportCard reportCard2 = new ReportCard("Brown", 20, 75);
+			}
+			catch (ArgumentException e)
+			{
+				WriteLine("{0} {1}", "reportCard2", e.Message);
+			}
+
+			try
+			{
+				ReportCard reportCard3 = new ReportCard("Cook", 100, 99);
+			}
+			catch (ArgumentException e)
+			{
+				WriteLine("{0} {1}", "reportCard3", e.Message);
+			}
+
+			try
+			{
+				ReportCard reportCard4 = new ReportCard("Dee", -1, 85);
+			}
+			catch (ArgumentException e)
+			{
+				WriteLine("{0} {1}", "reportCard4", e.Message);
+			}
+
+			try
+			{
+				ReportCard reportCard5 = new ReportCard("Edwards", 80, 101);
+			}
+			catch (ArgumentException e)
+			{
+				WriteLine("{0} {1}", "reportCard5", e.Message);
+			}
+		}
+	}
+}
---
+++ Chapter 11/Exercise 11-4/Exercise 11-4/ReportCard.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise_11_4
+{
+	class ReportCard
+	{
+		private string name;
+		private int midterm;
+		private int final;
+		private double average;
+		private char grade = 'F';
+		public const int LOWVALID = 0;
+		public const int HIGHVALID = 100;
+		public int[] CUTOFFS = { 90, 80, 70, 60 };
+		public char[] GRADE = { 'A', 'B', 'C', 'D' };
+		public ReportCard(string student, int mid, int fin)
+		{
+			if (mid < LOWVALID || mid > HIGHVALID)
+				throw (new ArgumentException());
+			if (fin < LOWVALID || fin > HIGHVALID)
+				throw (new ArgumentException());
+			average = (mid + fin) / 2.0;
+			for (int i = CUTOFFS.Length - 1; i >= 0 ; i--)
+			{
+				if (average > CUTOFFS[i])
+					grade = GRADE[i];
+			}
+			name = student;
+			midterm = mid;
+			final = fin;
+			WriteLine("{0} report card created.", name);
+			WriteLine("\tMidterm: {0} Final: {0}", midterm, final);
+			WriteLine("\tAverage: {0} Grade: {0}", average, grade);
+		}
+	}
+}
